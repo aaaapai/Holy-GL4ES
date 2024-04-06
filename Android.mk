bbe6cceb84863d340727d1c50f6c181454f4660e
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-04-06 09:11:11 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -88,12 +88,15 @@ LOCAL_SRC_FILES := \
 	src/glx/lookup.c \
 	src/glx/gbm.c \
 	src/glx/streaming.c \
+	src/gl/vgpu/shaderconv.c \
+	src/gl/vgpu/pack/pack.c \
+	src/gl/vgpu/pack/load.c \
+	src/gl/vgpu/pack/Initialization.c \
 
 LOCAL_CFLAGS += -g -funwind-tables -Ofast -fvisibility=hidden -include include/android_debug.h
 LOCAL_CFLAGS += -DNOX11
 LOCAL_CFLAGS += -DNO_GBM
 #LOCAL_CFLAGS += -DNO_INIT_CONSTRUCTOR
-LOCAL_CFLAGS += -DDEFAULT_ES=2
 LOCAL_CFLAGS += -DDEFAULT_ES=3
 LOCAL_CFLAGS += -DANDROID -pipe -integrated-as -fno-plt -Ofast -flto -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce
 //TODO: maybe temporary?
