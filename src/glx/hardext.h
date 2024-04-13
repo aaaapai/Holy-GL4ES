a5d05b9d3f7140e3e077fa008ef2e74c81c7b63b
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-04-13 23:08:55 +0800

    Update hardext.h

--- src/glx/hardext.h
+++ src/glx/hardext.h
@@ -73,6 +73,7 @@ typedef struct _hardext {
     int glsl120;        // does version 120 glsl shader are supported ?
     int glsl300es;      // does version 300es glsl shader are supported ?
     int glsl310es;      // does version 300es glsl shader are supported ?
+    int glsl320es;      // does version 300es glsl shader are supported ?
 } hardext_t;
 
 EXPORT extern hardext_t hardext;
