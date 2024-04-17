dbfd9cc480b92209272ef51908d73084f55544c3
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-04-17 12:48:38 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -83,16 +83,18 @@ LOCAL_SRC_FILES := \
 	src/gl/wrap/gles.c \
 	src/gl/wrap/glstub.c \
 	src/gl/math/matheval.c \
+        src/egl/egl.c \
+        src/egl/lookup.c
 	src/glx/hardext.c \
 	src/glx/glx.c \
 	src/glx/lookup.c \
 	src/glx/gbm.c \
 	src/glx/streaming.c
-LOCAL_CFLAGS +=  -DANDROID -pipe -integrated-as -fno-plt -Ofast -flto -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -g -std=gnu17 -funwind-tables -Ofast -fvisibility=hidden -include include/android_debug.h
+LOCAL_CFLAGS +=  -DANDROID -pipe -integrated-as -fno-plt -Ofast -flto -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -std=gnu17 -funwind-tables -Ofast -fvisibility=hidden -include include/android_debug.h
 LOCAL_CFLAGS += -DNOX11
 LOCAL_CFLAGS += -DNO_GBM
 #LOCAL_CFLAGS += -DNO_INIT_CONSTRUCTOR
-LOCAL_CFLAGS += -DDEFAULT_ES=3
+LOCAL_CFLAGS += -DDEFAULT_ES=2
 //TODO: maybe temporary?
 LOCAL_CFLAGS += -Wno-typedef-redefinition -Wno-dangling-else
 
@@ -102,19 +104,3 @@ LOCAL_LDLIBS := -ldl -llog
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-
-LOCAL_MODULE := gl4es_egl
-
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
-
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES) -DBCMHOST
-
-LOCAL_SRC_FILES := \
-	src/egl/egl.c \
-        src/egl/lookup.c
-
-LOCAL_CFLAGS += -DANDROID -pipe -integrated-as -fno-plt -Ofast -flto -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -g -std=gnu17 -funwind-tables -Ofast -fvisibility=hidden
-
-LOCAL_SHARED_LIBRARIES := gl4es_114_ptitseb
-
-include $(BUILD_SHARED_LIBRARY)
